ca70018752a2ca98a01b912520955714e4564ac1
tranhoa2602 <thdblacksky@gmail.com>
2024-07-19 15:56:47 +0700

    change: update

--- src/Components/layouts/Main.jsx
+++ src/Components/layouts/Main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "../../styles/layouts/main.css";
-import User from "../User/index";
+// import User from "../User/index";
 import EditUser from "../User/EditUser";
 import { Route, Routes } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 const Main = () => {
   return (
     <>
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<User />} />
+          <Route path="/Sidebar" element={<Sidebar />} />
           <Route path="/edit-user/:id" element={<EditUser />} />
         </Routes>
       </main>

db15e67014a3b70590ff0076fa91016b7a6c64ba
tranhoa2602 <thdblacksky@gmail.com>
2024-07-22 09:00:19 +0700

    Change: update new code

--- src/Components/Employee/Employee_Information/CreateEmployee.jsx
+++ src/Components/Employee/Employee_Information/CreateEmployee.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Button,
   Cascader,
@@ -9,7 +9,7 @@ import {
   Mentions,
   Select,
   TreeSelect,
-} from 'antd';
+} from "antd";
 
 const { RangePicker } = DatePicker;
 
@@ -25,14 +25,11 @@ const formItemLayout = {
 };
 
 const Create = () => (
-  <Form
-    {...formItemLayout}
-    style={{height: '100vh'}}
-  >
+  <Form {...formItemLayout} style={{ height: "100vh" }}>
     <Form.Item
       label="User Name"
       name="username"
-      rules={[{ required: true, message: 'Please input!' }]}
+      rules={[{ required: true, message: "Please input!" }]}
     >
       <Input />
     </Form.Item>
@@ -40,15 +37,15 @@ const Create = () => (
     <Form.Item
       label="Unkniown"
       name="InputNumber"
-      rules={[{ required: true, message: 'Please input!' }]}
+      rules={[{ required: true, message: "Please input!" }]}
     >
-      <InputNumber style={{ width: '100%' }} />
+      <InputNumber style={{ width: "100%" }} />
     </Form.Item>
 
     <Form.Item
       label="TextArea"
       name="TextArea"
-      rules={[{ required: true, message: 'Please input!' }]}
+      rules={[{ required: true, message: "Please input!" }]}
     >
       <Input.TextArea />
     </Form.Item>
@@ -56,7 +53,7 @@ const Create = () => (
     <Form.Item
       label="Mentions"
       name="Mentions"
-      rules={[{ required: true, message: 'Please input!' }]}
+      rules={[{ required: true, message: "Please input!" }]}
     >
       <Mentions />
     </Form.Item>
@@ -64,17 +61,18 @@ const Create = () => (
     <Form.Item
       label="Role"
       name="Select"
-      rules={[{ required: true, message: 'Please input!' }]}
+      rules={[{ required: true, message: "Please input!" }]}
     >
-      <Select onChange={onChange}>
+      <Select>
         <Select.Option value="admin">Admin</Select.Option>
+        <Select.Option value="employee">Employee</Select.Option>
       </Select>
     </Form.Item>
 
     <Form.Item
       label="Cascader"
       name="Cascader"
-      rules={[{ required: true, message: 'Please input!' }]}
+      rules={[{ required: true, message: "Please input!" }]}
     >
       <Cascader />
     </Form.Item>
@@ -82,7 +80,7 @@ const Create = () => (
     <Form.Item
       label="TreeSelect"
       name="TreeSelect"
-      rules={[{ required: true, message: 'Please input!' }]}
+      rules={[{ required: true, message: "Please input!" }]}
     >
       <TreeSelect />
     </Form.Item>
@@ -90,7 +88,7 @@ const Create = () => (
     <Form.Item
       label="DatePicker"
       name="DatePicker"
-      rules={[{ required: true, message: 'Please input!' }]}
+      rules={[{ required: true, message: "Please input!" }]}
     >
       <DatePicker />
     </Form.Item>
@@ -98,14 +96,12 @@ const Create = () => (
     <Form.Item
       label="RangePicker"
       name="RangePicker"
-      rules={[{ required: true, message: 'Please input!' }]}
+      rules={[{ required: true, message: "Please input!" }]}
     >
       <RangePicker />
     </Form.Item>
 
-    <Form.Item
-      wrapperCol={{ offset: 6, span: 16 }}
-    >
+    <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
       <Button type="primary" htmlType="submit">
         Submit
       </Button>

5569104dfeaa01c804e3114f9936f2c1e3864205
tranhoa2602 <thdblacksky@gmail.com>
2024-07-22 10:23:38 +0700

    Feat: change code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import "./styles/App.css";
 import Sidebar from "./Components/layouts/Sidebar";
 import Main from "./Components/layouts/Main";
-import Login from "./pages/Login";
-import { Route, Routes, useLocation } from "react-router-dom";
 
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 function App() {
   const location = useLocation();
   const showSidebar = location.pathname !== "/Login";
 
   return (
     <>
-      <Routes>
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-      {showSidebar && <Sidebar />}
+      <Sidebar />
 
       <Main />
     </>
--- src/Components/layouts/Main.jsx
+++ src/Components/layouts/Main.jsx
@@ -5,16 +5,18 @@ import EditUser from "../User/EditUser";
 import { Route, Routes } from "react-router-dom";
 import Create from "../Employee/Employee_Information/CreateEmployee";
 import Sidebar from "./Sidebar";
+import Login from "../../pages/Login";
 
 const Main = () => {
   return (
     <>
       <main className="main-content">
         <Routes>
-          <Route path="/Sidebar" element={<Sidebar />} />
+          {/* <Route path="/Sidebar" element={<Sidebar />} /> */}
           <Route path="/edit-user/:id" element={<EditUser />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/create" element={<Create />} />
         </Routes>
-        <Create></Create>
       </main>
     </>
   );

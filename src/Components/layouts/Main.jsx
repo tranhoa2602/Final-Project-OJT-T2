b4df3b197eabb0ed261931eb405bea3f23364f50
tranhoa2602 <thdblacksky@gmail.com>
2024-07-23 11:27:18 +0700

    Change: update new code

--- src/Components/layouts/Main.jsx
+++ src/Components/layouts/Main.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "../../styles/layouts/main.css";
-import EditUser from "../User/EditUser";
+// import EditUser from "../User/EditUser";
 import { Route, Routes } from "react-router-dom";
 import Create from "../Employee/Employee_Information/CreateEmployee";
 import Login from "../../pages/Login";
+import Admin from "../../pages/Admin";
 
 const Main = () => {
   const [user, setUser] = useState(null);
@@ -11,10 +12,10 @@ const Main = () => {
   return (
     <main className="main-content">
       <Routes>
-        <Route path="/edit-user/:id" element={<EditUser />} />
         <Route path="/" element={<Login setUser={setUser} />} />
+        <Route path="/admin" element={<Admin />} />
+        {/* <Route path="/edit-user/:id" element={<EditUser />} /> */}
         <Route path="/create" element={<Create />} />
-        {/* <Route path="/admin" element={<Admin />} /> */}
       </Routes>
     </main>
   );

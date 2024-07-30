46f906bc93ef1f8b937ef60df3259733093636d0
tranhoa2602 <thdblacksky@gmail.com>
2024-07-30 15:41:45 +0700

    change

--- src/Components/Employee/Employee_Information/EmployeeList.jsx
+++ src/Components/Employee/Employee_Information/EmployeeList.jsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { Space, Table, Tag, Button } from "antd";
+import { Space, Table, Tag, Button, Input } from "antd"; // Import Input from Ant Design
 import { useNavigate } from "react-router-dom";
 import { useEmployees } from "./EmployeeContext";
 import { getDatabase, ref, get } from "firebase/database";
 import ExcelJS from "exceljs";
 import { saveAs } from "file-saver";
-
-
+import {
+  EditOutlined,
+  DeleteOutlined,
+  InfoCircleOutlined,
+  FileExcelOutlined,
+} from "@ant-design/icons";
+import styles from "../../../styles/layouts/EmployeeList.module.scss"; // Import the SCSS module
 
 const columns = (handleEdit, handleDelete, navigate, positions, projects) => [
   {
     title: "Name",
     dataIndex: "name",
     key: "name",
-    render: (text, record) => <a onClick={() => navigate('/details', { state: { employee: record } })}>{text}</a>,
+    render: (text, record) => (
+      <a onClick={() => navigate("/details", { state: { employee: record } })}>
+        {text}
+      </a>
+    ),
   },
   {
     title: "Email",
@@ -30,7 +39,8 @@ const columns = (handleEdit, handleDelete, navigate, positions, projects) => [
     title: "Projects",
     dataIndex: "projectIds",
     key: "projectIds",
-    render: (projectIds) => projectIds.map(id => projects[id]?.name || "N/A").join(', '),
+    render: (projectIds) =>
+      projectIds.map((id) => projects[id]?.name || "N/A").join(", "),
   },
   {
     title: "Status",
@@ -56,18 +66,39 @@ const columns = (handleEdit, handleDelete, navigate, positions, projects) => [
     },
   },
   {
-    title: "Action",
-    key: "action",
+    title: "Actions",
+    key: "actions",
+    align: "center",
     render: (_, record) => (
-      <Space size="middle">
-        <a onClick={() => navigate('/edit', { state: { employee: record } })}>Edit</a>
-        <a onClick={() => navigate('/details', { state: { employee: record } })}>Details</a>
-        <a onClick={() => handleDelete(record.key)}>Delete</a>
-      </Space>
+      <div className={styles["actions-container"]}>
+        <Button
+          onClick={() => navigate("/edit", { state: { employee: record } })}
+          type="primary"
+          icon={<EditOutlined />}
+          className={styles["edit-button"]}
+        >
+          Edit
+        </Button>
+        <Button
+          type="default"
+          onClick={() => navigate("/details", { state: { employee: record } })}
+          icon={<InfoCircleOutlined />}
+          className={styles["detail-button"]}
+        >
+          Details
+        </Button>
+        <Button
+          type="danger"
+          onClick={() => handleDelete(record.key)}
+          icon={<DeleteOutlined />}
+          className={styles["delete-button"]}
+        >
+          Delete
+        </Button>
+      </div>
     ),
   },
 ];
-
 
 const fetchData = async () => {
   const db = getDatabase();
@@ -75,32 +106,28 @@ const fetchData = async () => {
   const positionsRef = ref(db, "positions");
   const projectsRef = ref(db, "projects");
 
-  const [employeesSnapshot, positionsSnapshot, projectsSnapshot] = await Promise.all([
-    get(employeesRef),
-    get(positionsRef),
-    get(projectsRef),
-  ]);
+  const [employeesSnapshot, positionsSnapshot, projectsSnapshot] =
+    await Promise.all([get(employeesRef), get(positionsRef), get(projectsRef)]);
 
   const employees = employeesSnapshot.exists() ? employeesSnapshot.val() : {};
   const positions = positionsSnapshot.exists() ? positionsSnapshot.val() : {};
   const projects = projectsSnapshot.exists() ? projectsSnapshot.val() : {};
 
-  // Process data as needed, e.g., converting to array
   const employeesArray = Object.entries(employees).map(([key, value]) => ({
-    key, // This is the unique ID
+    key,
     ...value,
   }));
 
   return { employees: employeesArray, positions, projects };
 };
 
-
 const EmployeeList = () => {
   const [employees, setEmployees] = useState([]);
   const [positions, setPositions] = useState({});
   const [projects, setProjects] = useState({});
+  const [searchText, setSearchText] = useState("");
   const navigate = useNavigate();
-  const {handleEdit, handleDelete } = useEmployees();
+  const { handleEdit, handleDelete } = useEmployees();
 
   useEffect(() => {
     const fetchDataAndSetState = async () => {
@@ -115,9 +142,9 @@ const EmployeeList = () => {
 
   const handleDeleteAndRefresh = async (key) => {
     await handleDelete(key);
-    fetchEmployees();
+    const { employees } = await fetchData();
+    setEmployees(employees);
   };
-
 
   const exportToExcel = async () => {
     const workbook = new ExcelJS.Workbook();
@@ -148,11 +175,14 @@ const EmployeeList = () => {
         phone: employee.phone,
         status: employee.status,
         positionId: positions[employee.positionId]?.name || employee.positionId,
-        projectIds: (employee.projectIds || []).map(id => projects[id]?.name || id).join(", "),
+        projectIds: (employee.projectIds || [])
+          .map((id) => projects[id]?.name || id)
+          .join(", "),
         skills: employee.skills,
         contact: employee.contact,
         cv_skill: employee.cv_list[0]?.cv_skill || "",
-        work_position: employee.cv_list[0]?.cv_experience[0]?.work_position || "",
+        work_position:
+          employee.cv_list[0]?.cv_experience[0]?.work_position || "",
         time_work: employee.cv_list[0]?.cv_experience[0]?.time_work || "",
         description: employee.cv_list[0]?.cv_experience[0]?.description || "",
         cv_file: employee.cv_file,
@@ -160,33 +190,49 @@ const EmployeeList = () => {
     });
 
     const buffer = await workbook.xlsx.writeBuffer();
-    const blob = new Blob([buffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+    const blob = new Blob([buffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
     saveAs(blob, "employee-list.xlsx");
   };
 
   return (
-    <div>
-      <Table 
-         columns={columns(null, handleDelete, navigate, positions, projects)}  
-        dataSource={employees} 
-        id="employee-table" 
+    <div className={styles["employee-list"]}>
+      <Space className={styles["actions-container"]}>
+        <Input
+          placeholder="Search by Name"
+          onChange={(e) => setSearchText(e.target.value)}
+          className={styles["search-input"]}
+        />
+        <Button
+          type="primary"
+          onClick={() => navigate("/create")}
+          className={styles["add-button"]}
+        >
+          Add Employee
+        </Button>
+        <Button
+          type="primary"
+          icon={<FileExcelOutlined />}
+          onClick={exportToExcel}
+          className={styles["export-button"]}
+        >
+          Export to Excel
+        </Button>
+      </Space>
+      <Table
+        columns={columns(
+          null,
+          handleDeleteAndRefresh,
+          navigate,
+          positions,
+          projects
+        )}
+        dataSource={employees}
+        rowKey="key"
+        pagination={{ pageSize: 6 }}
+        className={styles["employee-table"]}
       />
-      <Button
-        type="primary"
-        htmlType="button"
-        style={{ marginBottom: 16 }}
-        onClick={() => navigate("/create")}
-      >
-        Add Employee
-      </Button>
-      <Button
-        type="primary"
-        htmlType="button"
-        onClick={exportToExcel}
-        style={{ marginTop: 16, marginLeft: 16 }}
-      >
-        Export to Excel
-      </Button>
     </div>
   );
 };

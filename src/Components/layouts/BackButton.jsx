3715da4768fd8a86b79f3d1a777cca63adccf74b
tranhoa2602 <thdblacksky@gmail.com>
2024-08-07 17:33:12 +0700

    change

--- src/Components/layouts/BackButton.jsx
+++ src/Components/layouts/BackButton.jsx
@@ -1,24 +1,21 @@
-// BackButton.jsx
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
-import "../../styles/layouts/main.css"; // Import file CSS
+import { useTranslation } from "react-i18next";
+import "../../styles/layouts/main.css";
 
 const BackButton = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const handleBack = () => {
     navigate(-1); // Navigate -1 sẽ quay lại trang trước đó
   };
 
   return (
-    <Button
-      onClick={handleBack}
-      icon={<ArrowLeftOutlined />}
-      className="back-button"
-    >
-      {t("Back")}
+    <Button onClick={handleBack} className="back-button">
+      <ArrowLeftOutlined /> {t("Back")}
     </Button>
   );
 };
